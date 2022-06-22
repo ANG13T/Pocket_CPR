7a2c5f8edd3339bfc2bdc61cf9f3bae5f4ad4170
angelina-tsuboi <angelina.t1832@gmail.com>
2022-06-21 22:00:34 -0700

    Init traditional timer

--- CPR_Buddy WatchKit Extension/Views/TraditionalTimer.swift
+++ CPR_Buddy WatchKit Extension/Views/TraditionalTimer.swift
@@ -13,9 +13,15 @@ struct TraditionalTimerView: View {
     @State private var cycles: Int = 5
     @State private var loop: Bool = false
     @State private var selection: Tab = .tabOne
+    @State private var timer: Timer?
+    @State private var count = 0
+    @State private var wave: Bool = false
+    @State private var wave1: Bool = false
+    
        enum Tab {
            case tabOne
            case tabTwo
+           case tabThree
        }
      
     var body: some View {
@@ -54,11 +60,25 @@ struct TraditionalTimerView: View {
                     }.frame(height: 70).padding(.top, 40).padding(.bottom, 5)
                     
                     Button("Start Cycles") {
-                        isPresented.toggle()
+                        selection = Tab.tabThree
+                        loop = false
+                        count = 60
+                        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {time in
+                            if count > 0 {
+                                count -= 1
+                            }else if count <= 0 && cycles > 0{
+                                cycles -= 1
+                                count = 60
+                            }else {
+                                timer?.invalidate()
+                            }
+                        }
+
                     }.buttonStyle(BorderedButtonStyle(tint: .orange)).padding(.bottom, 5)
                     
                     Button("Loop Timer") {
-                        isPresented.toggle()
+                        selection = Tab.tabThree
+                        loop = true
                     }.buttonStyle(BorderedButtonStyle(tint: .pink))
                 
                 }
@@ -66,7 +86,16 @@ struct TraditionalTimerView: View {
 
             }.gesture(DragGesture()).tag(Tab.tabTwo)
             
-            
+            VStack {
+                ZStack {
+                    Circle().stroke(lineWidth: 20).frame(width: 60, height: 60).foregroundColor(.blue).scaleEffect(wave ? 2 : 1).opacity(wave ? 0 : 1).animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: false).speed(0.5)).onAppear() {
+                        self.wave.toggle()
+                    }
+                    Circle().frame(width: 60, height: 60).foregroundColor(.blue).shadow(radius: 25)
+                    Text("\(count)").font(.system(size: 40)).foregroundColor(.white).shadow(radius: 25)
+                }
+            }.gesture(DragGesture()).tag(Tab.tabThree)
+
             
 
         }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
