267588fdeaa5246d08feedae56ff08df38384771
angelina-tsuboi <angelina.t1832@gmail.com>
2022-06-29 12:09:57 -0700

    Init Settings Button

--- CPR_Buddy WatchKit Extension/ContentView.swift
+++ CPR_Buddy WatchKit Extension/ContentView.swift
@@ -11,67 +11,89 @@ struct ContentView: View {
     @State private var presentAboutView = false
     @State private var presentHandsOnlyView = false
     @State private var presentTraditionalView = false
+    @State private var presentSettingsView = false
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 15) {
-                Button("Hands-Only CPR") {
-                    presentHandsOnlyView.toggle()
-                }.buttonStyle(BorderedButtonStyle(tint: .blue))
-                    .fullScreenCover(isPresented: $presentHandsOnlyView) {
-                        HandsOnlyView(isPresented: $presentHandsOnlyView).toolbar {
-                            
-                            ToolbarItem(placement: .cancellationAction) {
+            ScrollView {
+                VStack(spacing: 15) {
+                    Button("Hands-Only CPR") {
+                        presentHandsOnlyView.toggle()
+                    }.buttonStyle(BorderedButtonStyle(tint: .blue))
+                        .fullScreenCover(isPresented: $presentHandsOnlyView) {
+                            HandsOnlyView(isPresented: $presentHandsOnlyView).toolbar {
                                 
-                                Button(action: {
-                                    presentHandsOnlyView.toggle()
-                                }) {
-                                    Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                ToolbarItem(placement: .cancellationAction) {
+                                    
+                                    Button(action: {
+                                        presentHandsOnlyView.toggle()
+                                    }) {
+                                        Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                    }
+                                    
                                 }
                                 
                             }
-                            
-                        }
-                    }.padding(.top, 25)
-                
-                Button("Traditional CPR") {
-                    presentTraditionalView.toggle()
-                }.buttonStyle(BorderedButtonStyle(tint: .pink))
-                    .fullScreenCover(isPresented: $presentTraditionalView) {
-                        TraditionalView(isPresented: $presentTraditionalView).toolbar {
-                            
-                            ToolbarItem(placement: .cancellationAction) {
+                        }.padding(.top, 10)
+                    
+                    Button("Traditional CPR") {
+                        presentTraditionalView.toggle()
+                    }.buttonStyle(BorderedButtonStyle(tint: .pink))
+                        .fullScreenCover(isPresented: $presentTraditionalView) {
+                            TraditionalView(isPresented: $presentTraditionalView).toolbar {
                                 
-                                Button(action: {
-                                    presentTraditionalView.toggle()
-                                }) {
-                                    Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                ToolbarItem(placement: .cancellationAction) {
+                                    
+                                    Button(action: {
+                                        presentTraditionalView.toggle()
+                                    }) {
+                                        Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                    }
+                                    
                                 }
                                 
                             }
-                            
                         }
-                    }
-                
-                
-                Button("About CPR") {
-                    presentAboutView.toggle()
-                }.buttonStyle(BorderedButtonStyle(tint: .orange))
-                    .fullScreenCover(isPresented: $presentAboutView) {
-                        AboutView(isPresented: $presentAboutView).toolbar {
-                            
-                            ToolbarItem(placement: .cancellationAction) {
+                    
+                    
+                    Button("About CPR") {
+                        presentAboutView.toggle()
+                    }.buttonStyle(BorderedButtonStyle(tint: .orange))
+                        .fullScreenCover(isPresented: $presentAboutView) {
+                            AboutView(isPresented: $presentAboutView).toolbar {
                                 
-                                Button(action: {
-                                    presentAboutView.toggle()
-                                }) {
-                                    Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                ToolbarItem(placement: .cancellationAction) {
+                                    
+                                    Button(action: {
+                                        presentAboutView.toggle()
+                                    }) {
+                                        Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                    }
+                                    
                                 }
                                 
                             }
-                            
                         }
-                    }
+                    
+                    Button("Settings") {
+                        presentSettingsView.toggle()
+                    }.buttonStyle(BorderedButtonStyle(tint: .green))
+                        .fullScreenCover(isPresented: $presentSettingsView) {
+                            AboutView(isPresented: $presentSettingsView).toolbar {
+                                
+                                ToolbarItem(placement: .cancellationAction) {
+                                    
+                                    Button(action: {
+                                        presentSettingsView.toggle()
+                                    }) {
+                                        Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                                    }
+                                    
+                                }
+                                
+                            }
+                        }
+                }
             }
         }
     }
