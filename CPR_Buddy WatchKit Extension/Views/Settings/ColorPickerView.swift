2bad82a6bb35acc8eea0daf3930b6e5e69536c0e
angelina-tsuboi <angelina.t1832@gmail.com>
2022-06-29 16:19:33 -0700

    feat: open color picker view

---
+++ CPR_Buddy WatchKit Extension/Views/Settings/ColorPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPickerView.swift
+//  Pocket_CPR WatchKit Extension
+//
+//  Created by Angelina Tsuboi on 6/29/22.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+
--- CPR_Buddy WatchKit Extension/Views/Settings/SettingsView.swift
+++ CPR_Buddy WatchKit Extension/Views/Settings/SettingsView.swift
@@ -9,24 +9,28 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var isPresented: Bool
+    @State var presentColorPicker: Bool = false
     @State private var compressionRate = 100.0
     @State private var colorScheme = 1
     @State private var selectedColor : Color = .blue
     
     var body: some View {
         VStack {
-            Text("Compressions Per Minute: \(compressionRate)")
-                .foregroundColor(.blue)
+            
+            Text("Settings").bold().font(.system(size: 20))
             Slider(
                 value: $compressionRate,
                 in: 100...120,
                 step: 1
             )
             
+            Text("Compressions Per Minute: \(compressionRate)")
+                .foregroundColor(.blue).font(.system(size: 20))
+            
             VStack {
                 List {
                     HStack {
-                        Text("Color")
+                        Text("Pulse Color")
                         
                         Spacer()
                         
@@ -34,13 +38,25 @@ struct SettingsView: View {
                     }
                 }
                 .foregroundColor(Color.white)
-            }
+            }.fullScreenCover(isPresented: $presentColorPicker) {
+                TraditionalTutorialView(isPresented: $presentColorPicker).toolbar {
+                    
+                    ToolbarItem(placement: .cancellationAction) {
+                        
+                        Button(action: {
+                            presentColorPicker.toggle()
+                        }) {
+                            Image(systemName: "arrow.left.circle.fill").foregroundColor(.gray)
+                        }
+                        
+                    }
+                    
+                }
+            }.padding(.horizontal).buttonStyle(BorderedButtonStyle(tint: .pink)).padding(.top, 5)
             
             
-//           Picker("Color Scheme", selection: $colorScheme) {
-//               Text("Light").tag(0)
-//               Text("Dark").tag(1)
-//           }.pickerStyle(InlinePickerStyle())
+            Text("Vibrations")
+            
         }
     }
 }
